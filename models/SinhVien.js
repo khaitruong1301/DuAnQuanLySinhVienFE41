2dc0311242a9a951746a731978c794ce4b0b43ea
khai <khaitruong1301@gmail.com>
2020-06-11 19:45:23 +0700

    'upload_code'

--- models/SinhVien.js
+++ models/SinhVien.js
@@ -6,4 +6,10 @@ var SinhVien = function (){
     this.DiemToan = '';
     this.DiemLy = '';
     this.DiemHoa = '';
+
+
+    this.xuatThongTinSinhVien = function () {
+        console.log(this.TenSV);
+        console.log(this.Email);
+    }
 }
